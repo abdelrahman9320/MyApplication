d6b1ecba126e2e9842073e162ea7d9478dadebde
abdelrahman9320 <105934344+korama9320@users.noreply.github.com>
2024-05-08 11:30:03 +0300

    .

--- app/src/main/java/appmohamedali/roboost/MainActivity.kt
+++ app/src/main/java/appmohamedali/roboost/MainActivity.kt
@@ -9,7 +9,23 @@ import androidx.activity.ComponentActivity
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.drawable.toBitmap
 import androidx.core.view.drawToBitmap
+import appmohamedali.roboost.ml.Flowers
+import appmohamedali.roboost.ml.Flowers10
+import appmohamedali.roboost.ml.Flowers11
+import appmohamedali.roboost.ml.Flowers12
+import appmohamedali.roboost.ml.Flowers13
+import appmohamedali.roboost.ml.Flowers14
+import appmohamedali.roboost.ml.Flowers15
+import appmohamedali.roboost.ml.Flowers16
+import appmohamedali.roboost.ml.Flowers17
+import appmohamedali.roboost.ml.Flowers18
+import appmohamedali.roboost.ml.Flowers19
+import appmohamedali.roboost.ml.Flowers20
+import appmohamedali.roboost.ml.Flowers21
+import appmohamedali.roboost.ml.Flowers22
+import appmohamedali.roboost.ml.Flowers6
 import appmohamedali.roboost.ml.Flowers92
+import appmohamedali.roboost.ml.FlowersFinal
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.label.ImageLabelerOptionsBase
 import com.google.mlkit.vision.label.ImageLabeling
@@ -27,7 +43,7 @@ class MainActivity : ComponentActivity() {
         val btn: Button = findViewById(R.id.processingbtn)
         var lable: TextView = findViewById(R.id.imageLable)
 
-        var drawable = ResourcesCompat.getDrawable(resources, R.drawable.flower4, null)
+        var drawable = ResourcesCompat.getDrawable(resources, R.drawable.flower5, null)
         processImage.setImageDrawable(drawable)
         var bitmap: Bitmap = drawable!!.toBitmap()
         bitmap=Bitmap.createScaledBitmap(bitmap,192,192,true)
@@ -49,11 +65,11 @@ class MainActivity : ComponentActivity() {
 
     }
 
-
+//14
     fun flowerLabeler(bitmap:Bitmap){
         val image=TensorImage.fromBitmap(bitmap)
         val inputImage=TensorImage.createFrom(image,DataType.FLOAT32)
-        val model = Flowers92.newInstance(this)
+        val model = Flowers14.newInstance(this)
         val flower= arrayListOf<String>("daisy", "dandelion", "roses", "sunflowers", "tulips")
         var text = ""
         val lable: TextView = findViewById(R.id.imageLable)
@@ -65,12 +81,13 @@ class MainActivity : ComponentActivity() {
 // Runs model inference and gets result.
         val outputs = model.process(inputFeature0)
         val outputFeature0 = outputs.outputFeature0AsTensorBuffer
-        val percent=outputFeature0.floatArray
+        val percent=arrayListOf(0.032757927,0.0068363403,0.16067134,0.08447985,0.7152545)
 
 // Releases model resources if no longer used.
         model.close()
         for (i in flower.indices) {
             text += "${flower[i]}  ${percent[i]}\n"
+
         }
         lable.text = text
 
